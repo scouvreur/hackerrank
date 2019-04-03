3baf80a50c92626c8b0873d41ca2623ceba9c893
Stephane Couvreur <stephane.couvreur@feedzai.com>
2019-04-03 23:03:19 +0100

    [30D-Code] [Day 3: Intro to Conditional Statements] Solution added

---
+++ 30-days-of-code/python/day_3.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+N = int(input())
+
+if N % 2 == 1:
+    print("Weird")
+elif N >= 2 and N <= 5:
+    print("Not Weird")
+elif N >= 6 and N <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
