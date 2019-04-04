235afefb8ddbfd54b11aef5a14faf7057a6596d9
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-04-04 01:43:05 +0100

    [30D-Code] [Day 7: Arrays] Solution added

---
+++ 30-days-of-code/python/day_7.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def invert_list(numbers):
+    """
+    Parameters
+    ----------
+    numbers : list
+        a list of numbers
+    
+    Returns
+    -------
+    reversed : list
+        the reverse list of numbers passed
+    """
+    reversed = []
+    for i in range(len(numbers)):
+        reversed.append(numbers.pop())
+    return reversed
+
+if __name__ == '__main__':
+    n = int(input())
+
+    array = list(map(lambda x: int(x), input().split()))
+    reversed = invert_list(array)
+    print(" ".join(map(str, reversed)))
