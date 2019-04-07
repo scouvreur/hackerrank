f54c08d270f184dbc61c449aeffd3749a5406f4a
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-04-07 18:30:33 +0100

    [10D-Stats] [Day 1: Standard Deviation] Solution added

---
+++ 10-days-of-statistics/python/day_0_2.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+n = int(input())
+data = list(map(int, input().split()))
+weights = list(map(int, input().split()))
+
+def weighted_sum(data, weights):
+    assert len(data) == len(weights)
+    sum_weights = reduce(lambda x, y: x + y, weights)
+    cumsum = 0
+    for i in range(len(data)):
+        cumsum += data[i]*weights[i]
+    return round(cumsum/sum_weights, 1)
+
+print(weighted_sum(data, weights))
