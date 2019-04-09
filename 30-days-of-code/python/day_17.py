0263f8ad43dd24670bbcbf7738f9513855fbc574
Stephane Couvreur <stephane.couvreur@feedzai.com>
2019-04-09 08:08:45 +0100

    [30D-Code] [Python] [Day 17: More Exceptions] Solution added

---
+++ 30-days-of-code/python/day_17.py
@@ -0,0 +1,26 @@
+class NegativeValueError(Exception):
+   """Raised when the input value is negative"""
+   pass
+
+class Calculator():
+    def __init__(self):
+        pass
+
+    def power(self, n, p):
+        try:
+            if n < 0 or p < 0:
+                raise NegativeValueError
+            else:
+                return n**p
+        except NegativeValueError:
+            return "n and p should be non-negative"
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
