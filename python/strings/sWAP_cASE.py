0b752c573df037d53687405bc93e39f165b81a0e
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-05-30 14:35:51 +0100

    [Practice] [Python] [Strings] Solution added

---
+++ python/strings/sWAP_cASE.py
@@ -0,0 +1,28 @@
+def swap_case(string):
+    """
+    This function takes in a string and returns
+    a swapped version.
+
+    Parameters
+    ----------
+    string : string
+        The string to swap cases on.
+
+    Returns
+    -------
+    swapped : string
+        The swapped string.
+    """
+    swapped = ""
+    for char in string:
+        if char.islower():
+            swapped += char.upper()
+        else:
+            swapped += char.lower()
+    return swapped
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
