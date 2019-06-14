3d3d27850e3bf16a6e72b7377aa01b3e4bcc0ee3
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-06-14 14:40:09 +0100

    [Practice] [Python] [Math] Solution added

--- python/math/polar_coordinates.py
+++ python/math/polar_coordinates.py
@@ -1,8 +1,19 @@
 from cmath import phase
 
-number = input().split('+')
-number[1] = number[1].split('j')[0]
-number = complex(int(number[0]), int(number[1]))
+number = input()
+if '+' in number:
+    number = number.split('+')
+    number[1] = number[1].split('j')[0]
+    number = complex(int(number[0]), int(number[1]))
+elif '-' in number:
+    if number.count('-') == 1:
+        number = number.split('-')
+        number[1] = number[1].split('j')[0]
+        number = complex(int(number[0]), -int(number[1]))
+    elif number.count('-') == 2:
+        number = number.split('-')
+        number[2] = number[2].split('j')[0]
+        number = complex(-int(number[1]), -int(number[2]))
 
 print(abs(number))
 print(phase(number))
