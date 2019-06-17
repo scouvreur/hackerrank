90e19db57d47da3bbadfb6e787cff6ab2706409b
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-06-17 17:31:59 +0100

    [Practice] [Python] [Sets] Solution added

---
+++ python/sets/no_idea.py
@@ -0,0 +1,14 @@
+n = 3
+m = 2
+array = [1, 5, 3]
+A = set([3, 1])
+B = set([5, 7])
+
+happiness = 0
+for item in array:
+	if item in A:
+		happiness += 1
+	if item in B:
+		happiness -= 1
+
+print(happiness)
