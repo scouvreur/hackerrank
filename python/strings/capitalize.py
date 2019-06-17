37cbda85b1f70b1a51e9417c2df3958cab4b3d1d
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-06-17 16:56:26 +0100

    [Practice] [Python] [Strings] Solution added

--- python/strings/capitalize.py
+++ python/strings/capitalize.py
@@ -1,2 +1,18 @@
 name = "chris alan"
 name = name.title()
+
+name = "1 w 2 r 3g"
+
+# Complete the solve function below.
+def solve(string):
+	"""
+	Capitalizing function
+	"""
+	list_strings = string.rstrip().split(' ')
+	result = ""
+	for item in list_strings:
+		if item[0].isalpha():
+			item = item.title()
+
+		result += item + ' '
+	return result.strip()
