271667e8912636e8ff9ea22184c9b35dfea72e8e
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-06-17 15:25:26 +0100

    [Practice] [Python] [Strings] Solution added

---
+++ python/strings/text_wrap.py
@@ -0,0 +1,32 @@
+def wrap(string, max_width):
+    """
+    Takes in a string of length n and an integer
+    max_width < n, and returns the string wrapped
+    with lines no longer then max_width chars.
+
+    Parameters
+    ----------
+    string : string
+        Input string.
+
+    max_width : int
+        Max number of chars before inserting
+        newline.
+
+    Returns
+    -------
+    result : string
+        Wrapped string.
+    """
+    result = ""
+    for i in range(len(string)):
+    	try:
+    		result += string[i*max_width:(i+1)*max_width] + "\n"
+    	except IndexError:
+    		result += string[i*max_width:]
+    return result
+
+
+string, max_width = "ABCDEFGHIJKLIMNOQRSTUVWXYZ", 4
+result = wrap(string, max_width)
+print(result)
