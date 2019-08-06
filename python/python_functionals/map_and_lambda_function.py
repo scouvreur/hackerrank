5142db2a611a6c9ce4e3192e086eb86e5a363f0a
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-08-06 16:56:51 +0100

    [Practice] [Python] [Functionals] Solution added

---
+++ python/python_functionals/map_and_lambda_function.py
@@ -0,0 +1,24 @@
+cube = lambda x: x**3 # complete the lambda function
+
+def fibonacci(n):
+    """
+    Returns a list of fibonacci numbers of length n
+
+    Parameters
+    ----------
+    n : int
+        Number in fibonacci suite desired
+
+    Returns
+    -------
+    fib_list : list[ints]
+        List of integers
+    """
+    memo = [0, 1]
+
+    for i in range(2, n):
+        memo += [memo[i-2] + memo[i-1]]
+
+    return memo[:n]
+
+print(fibonacci(5))
