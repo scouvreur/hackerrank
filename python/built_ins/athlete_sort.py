8c8d042d67eb7e3d2a3269423f5267666929cb6b
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2020-10-06 13:32:15 +0100

    [Practice] [Python] [Built-ins] Solution added

---
+++ python/built_ins/athlete_sort.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+    k = int(input())
+
+
+arr_sorted = sorted(arr, key=lambda x: x[k])
+
+for row in arr_sorted:
+    print(' '.join(list(map(str, row))))
