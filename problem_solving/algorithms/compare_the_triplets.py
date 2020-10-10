6e6443430cc55ff0b373d11bf31e3762994dd9b2
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2020-10-10 17:31:19 +0100

    [Practice] [Problem solving] [Algorithms] Solution added

---
+++ problem_solving/algorithms/compare_the_triplets.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+from typing import List
+
+a = list(map(int, input().rstrip().split()))
+b = list(map(int, input().rstrip().split()))
+
+# Complete the compareTriplets function below.
+def compareTriplets(a: List[int], b: List[int]) -> List[int]:
+    score_a, score_b = 0, 0
+    assert len(a) == 3, "a is not a triplet"
+    assert len(b) == 3, "b is not a triplet"
+    for i in range(3):
+        if a[i] > b[i]:
+            score_a += 1
+        elif a[i] < b[i]:
+            score_b += 1
+    return [score_a, score_b]
+
+result = compareTriplets(a, b)
+print(' '.join(map(str, result)))
