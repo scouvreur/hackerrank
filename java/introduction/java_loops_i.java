ce48d63cf72848a9aad9f29a402259f4acfdabad
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2020-11-19 17:40:30 +0000

    [Practice] [Java] [Introduction] Solution added

---
+++ java/introduction/java_loops_i.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * @author Stephane Couvreur
+ */
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Integer n = scanner.nextInt();
+        // Solution here
+        scanner.close();
+    }
+}
