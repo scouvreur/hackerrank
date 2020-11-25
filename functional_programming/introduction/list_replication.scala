5ef54cf56f10aa03146c40f52fcc5ae191349c99
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2020-11-25 10:53:10 +0000

    [Functional Programming] [Scala] [Introduction] Solution added

---
+++ functional_programming/introduction/list_replication.scala
@@ -0,0 +1,17 @@
+object Solution {
+
+    def main(args: Array[String]) {
+        println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+    }
+
+    def f(n: Int) {
+        for (w <- 0 until n) {
+            println("duplicate array")
+        }
+    }
+
+    def f(num: Int, arr: List[Int]): List[Int] =
+
+    var N = scala.io.StdIn.readInt
+    f(N)
+}
