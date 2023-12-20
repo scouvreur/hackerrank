408175fd7ed9530864c428822f125ef5d6c27397
Stéphane Couvreur <stephane.couvreur.sueron@gmail.com>
2023-12-20 18:57:57 +0000

    [Certifications] Add problems from assessments

---
+++ certifications/problem_solving_basic/password_decryption.py
@@ -0,0 +1,50 @@
+def encrypt(input: str) -> str:
+    output = ""
+    i = 0
+    while i < len(input)-1:
+        if input[i].islower() and input[i+1].isupper():
+            output += input[i+1] + input[i] + "*"
+            i += 2
+        elif input[i].isdigit():
+            output = input[i] + output + "0"
+            i += 1
+        else:
+            output += input[i]
+            i += 1
+
+    output += input[i]
+    return output
+
+def test_encrypt():
+    """
+    Test for the encrypt function.
+    """
+    assert encrypt("hAck3rr4nk") == "43Ah*ck0rr0nk"
+    assert encrypt("aP1pL5e") == "51Pa*0Lp*0e"
+    assert encrypt("poTaTO") == "pTo*Ta*O"
+
+def decrypt(input: str) -> str:
+    reversed_input = list(input[::-1])
+
+    for i, char in enumerate(reversed_input):
+        # switch the zeros with the original numbers
+        if char == "0":
+            reversed_input[i] = reversed_input.pop()
+        # look for *
+        if char == "*" and i+2 < len(reversed_input):
+            reversed_input[i+1], reversed_input[i+2] = reversed_input[i+2], reversed_input[i+1]
+            reversed_input.pop(i)
+
+    output = "".join(reversed_input)[::-1]
+    return output
+
+def test_decrypt():
+    """
+    Test for the decrypt function.
+    """
+    assert decrypt("43Ah*ck0rr0nk") == "hAck3rr4nk"
+    assert decrypt("51Pa*0Lp*0e") == "aP1pL5e"
+    assert decrypt("pTo*Ta*O") == "poTaTO"
+
+test_encrypt()
+test_decrypt()
---
+++ certifications/problem_solving_basic/subarray_sums.py
@@ -0,0 +1,44 @@
+import random
+
+numbers = [5, 10, 10]
+
+numbers = [0, 10, 40, 90, 0, 60, 100, 10, 40, 50, 30, 0]
+queries = [[1, 4, 5]]
+
+query_results = []
+
+cumulative_sums = []
+cumulative_sum = 0
+zeros = []
+num_zeros = 0
+
+for number in numbers:
+    cumulative_sum += number
+    cumulative_sums.append(cumulative_sum)
+
+    if number == 0:
+        num_zeros += 1
+    zeros.append(num_zeros)
+
+
+for query in queries:
+    start_index = query[0] - 1
+    end_index = query[1]
+    zero_value = query[2]
+
+    count_zeros = zeros[end_index] - zeros[start_index]
+    subarray_sum = 0
+
+    for number in numbers[start_index:end_index]:
+        if number == 0:
+            count_zeros += 1
+        subarray_sum += number
+
+    subarray_sum += count_zeros * zero_value
+    query_results.append(subarray_sum)
+
+
+    # Generate an array of 10 random integers between 1 and 100
+    random_numbers = [random.randint(1, 100) for _ in range(10)]
+
+    print(random_numbers)
